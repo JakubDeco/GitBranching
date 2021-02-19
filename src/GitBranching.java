6efa4e2aa005f4f43bf6c19f7421de9300e8411a
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-19 14:39:12 +0100

    Update GitBranching.java
    changed

--- src/GitBranching.java
+++ src/GitBranching.java
@@ -1,5 +1,9 @@
 public class GitBranching {
     public void printHello(String name){
-        System.out.println("Hello "+name);
+        System.out.println("Hello , what is your name? Aha : "+name);
+    }
+    
+    public int calc(){
+        return 5*8+7;
     }
 }
